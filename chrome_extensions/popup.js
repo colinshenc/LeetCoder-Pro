c9e8959057e443c076ef3bdd46d030cb48294e89
Vincenqwu <94396401+Vincenqwu@users.noreply.github.com>
2021-12-27 18:04:06 -0800

    Add extension template for chrome

---
+++ chrome_extensions/popup.js
@@ -0,0 +1,13 @@
+// Initialize button to OFF 
+let switchButton = document.getElementById("switch");
+
+switchButton.addEventListener("click", function() {
+  if (switchButton.innerHTML == "ON") {
+    switchButton.innerHTML = "OFF";
+    // TODO
+  }
+  else {
+    switchButton.innerHTML = "ON";
+    // TODO
+  }
+});
