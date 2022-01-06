004fd772f9f5bf264dc0f80ebc44cbc23eb74491
cshen <cshen@student.ethz.ch>
2022-01-06 00:46:57 -0800

    delete

--- get_questions_on_currrent_page.js
+++
@@ -1,4 +0,0 @@
-//Gets an array of problems and numbers from current page.
-var problems_on_page = Array.from(document.querySelectorAll(".h-5"))
-  .filter((node) => node.innerHTML.match("\\d+\\.( \\w*)+"))
-  .map((node) => node.innerHTML);
