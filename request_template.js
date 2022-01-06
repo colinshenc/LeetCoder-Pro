f6153deba871b3526c807e7d0793bab102f2add5
cshen <cshen@student.ethz.ch>
2022-01-06 00:25:49 -0800

    delete files

--- request_template.js
+++
@@ -1,9 +0,0 @@
-fetch("https://leetcode.com/graphql", {
-  headers: {
-    "content-type": "application/json",
-  },
-  body: '{"operationName":"Submissions","variables":{"offset":0,"limit":20,"lastKey":null,"questionSlug":"CHANGE HERE TO QUESTION SLUG"},"query":"query Submissions($offset: Int!, $limit: Int!, $lastKey: String, $questionSlug: String!) {\\n  submissionList(offset: $offset, limit: $limit, lastKey: $lastKey, questionSlug: $questionSlug) {\\n    lastKey\\n    hasNext\\n    submissions {\\n      id\\n      statusDisplay\\n      lang\\n      runtime\\n      timestamp\\n      url\\n      isPending\\n      memory\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n"}',
-  method: "POST",
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data));
