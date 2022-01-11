054548b7bcacaeed010a9bbfbe0a4d3807dce62d
Qiuan Wu <QiuanWu@Qiuans-MacBook-Pro.local>
2022-01-10 16:20:58 -0800

    Fixed button issue

--- extensions/popup.js
+++ extensions/popup.js
@@ -1,5 +1,5 @@
 var storageAPI = chrome.storage.sync;
-if(navigator.userAgent.match(/chrome|chromium|crios/i)) {
+if (navigator.userAgent.match(/chrome|chromium|crios/i)) {
   storageAPI = chrome.storage.sync;
 } else if (navigator.userAgent.match(/firefox|fxios/i)) {
   storageAPI = browser.storage.local;
@@ -9,13 +9,18 @@ if(navigator.userAgent.match(/chrome|chromium|crios/i)) {
 storageAPI.get('buttonState', function (data) {
   let button = document.getElementById("switch");
   console.log('buttonState', data);
-  if (data['buttonState'] == 1) {
-    button.innerHTML = "ON";
-    button.style.backgroundColor = "rgba(35, 228, 60, 0.904)";
-  }
-  else {
-    button.innerHTML = "OFF";
-  }
+
+  chrome.cookies.getAll({ domain: ".leetcode.com", name: "LEETCODE_SESSION" }, (result) => {
+    if (result.length != 0) {
+      if (data['buttonState'] == 1) {
+        button.innerHTML = "ON";
+        button.style.backgroundColor = "rgba(35, 228, 60, 0.904)";
+      }
+      else {
+        button.innerHTML = "OFF";
+      }
+    }
+  });
 })
 
 // Add listener for button
@@ -24,32 +29,41 @@ document.addEventListener('DOMContentLoaded', function () {
   var isExtensionOn = 0;
   var obj = {}
   button.addEventListener("click", function () {
-    if (button.innerHTML == "ON") {
-      button.innerHTML = "OFF";
-      isExtensionOn = 0;
-      button.style.backgroundColor = "blanchedalmond";
-    }
-    else {
-      button.innerHTML = "ON";
-      isExtensionOn = 1;
-      button.style.backgroundColor = "rgba(35, 228, 60, 0.904)";
-    }
-    storageAPI.set({ 'buttonState': isExtensionOn }, function () {
-      console.log('Status saved');
-    })
-    chrome.tabs.query({url: "*://*.leetcode.com/problemset/*" }, function (tab) {
-      // If the page is not opened
-      console.log(tab);
-      if (tab[0] == null) {
-        console.log("Not found in opened tabs");
+    chrome.cookies.getAll({ domain: ".leetcode.com", name: "LEETCODE_SESSION" }, (result) => {
+      console.log(result);
+      if (result.length == 0) {
+        console.log("Undefined");
       }
       else {
-        console.log("Page reloaded");
-        for (var i = 0; i < tab.length;i++) {
-          chrome.tabs.reload(tab[i].id);
+        if (button.innerHTML == "ON") {
+          button.innerHTML = "OFF";
+          isExtensionOn = 0;
+          button.style.backgroundColor = "blanchedalmond";
         }
+        else {
+          button.innerHTML = "ON";
+          isExtensionOn = 1;
+          button.style.backgroundColor = "rgba(35, 228, 60, 0.904)";
+        }
+        storageAPI.set({ 'buttonState': isExtensionOn }, function () {
+          console.log('Status saved');
+        })
+        chrome.tabs.query({ url: "*://*.leetcode.com/problemset/*" }, function (tab) {
+          // If the page is not opened
+          console.log(tab);
+          if (tab[0] == null) {
+            console.log("Not found in opened tabs");
+          }
+          else {
+            console.log("Page reloaded");
+            for (var i = 0; i < tab.length; i++) {
+              chrome.tabs.reload(tab[i].id);
+            }
+          }
+        })
       }
-    })
+    });
+
   });
 })
 
